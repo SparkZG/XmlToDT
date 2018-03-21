f6a3155b2c2f38a44cebfb88cced05050e8eaa58
影子张 <18155371857@163.com>
2018-03-21 10:11:11 +0800

    修复软件不能正常运行的问题
    Microsoft.ACE.OLEDB.12.0是不能在x64上使用的，你要强制把你的application编译成x86

--- XmlToDT/Properties/Resources.Designer.cs
+++ XmlToDT/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace XML.Properties {
+namespace XmlToDT.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace XML.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("XML.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("XmlToDT.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- XmlToDT/Properties/Settings.Designer.cs
+++ XmlToDT/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace XML.Properties {
+namespace XmlToDT.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
